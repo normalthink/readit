609467542e1a6ea8b1a354d71c00022ccccd0ebc
normalthink <peter.thinknormal@gmail.com>
2024-03-13 14:32:28 +0900

    5-2 Optimization(다이나믹 캐싱 등)

--- src/components/post-vote/PostVoteServer.tsx
+++ src/components/post-vote/PostVoteServer.tsx
@@ -15,7 +15,13 @@ interface PostVoteServerProps {
  * fetching inside of this component, allowing for faster page loads via suspense streaming.
  * We also have to option to fetch this info on a page-level and pass it in.
  *
+ * PostVotes를 클라이언트와 서버 컴포넌트로 분리하여 
+ * 이 컴포넌트 내부에서 동적 데이터를 가져올 수 있도록 하여 
+ * 서스펜스 스트리밍을 통해 페이지를 더 빠르게 로드할 수 있도록 했습니다. 
+ * 또한 페이지 수준에서 이 정보를 가져와서 전달할 수 있는 옵션도 필요합니다.
+ * 
  */
+
 
 const PostVoteServer = async ({
   postId,
@@ -25,9 +31,11 @@ const PostVoteServer = async ({
 }: PostVoteServerProps) => {
   const session = await getAuthSession()
 
+  //we're going to default it to undefined as if the user has not voted yet and the reason we're declarig these  
   let _votesAmt: number = 0
   let _currentVote: Vote['type'] | null | undefined = undefined
 
+  // as let is to mutate them 
   if (getData) {
     // fetch data in component
     const post = await getData()
